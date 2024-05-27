28f6fec850dd053bed3fa12ed8646503c5ecce1d
Kishore N <kishikishore.2863@gmail.com>
2024-05-27 23:42:08 +0530

    organizing contact page

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -3,8 +3,6 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
 @Component({
   selector: 'app-contact-us',
   standalone: true,
@@ -13,19 +11,5 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrl: './contact-us.component.css',
 })
 export class ContactUsComponent {
-  myForm: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    this.myForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-    });
-  }
-
-  onSubmit() {
-    if (this.myForm.valid) {
-      // Form is valid, handle submission here
-      console.log(this.myForm.value);
-    }
-  }
+  toggle() {}
 }
