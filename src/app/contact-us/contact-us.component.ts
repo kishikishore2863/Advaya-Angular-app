c61009fbb0f9d7c200dc1dbbb3a3a41d9686d316
Kishore N <kishikishore.2863@gmail.com>
2024-05-28 17:16:55 +0530

    updated

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../footer/footer.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [RouterModule, CommonModule, ReactiveFormsModule],
+  imports: [RouterModule, CommonModule, ReactiveFormsModule, FooterComponent],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css',
 })
 export class ContactUsComponent {
-  toggle() {}
+  toggle(event: Event) {
+    const targetElement = event.target as HTMLElement;
+    const panel = targetElement.nextElementSibling;
+
+    if (panel && panel.classList.contains('panel')) {
+      panel.classList.toggle('active');
+    }
+  }
 }
